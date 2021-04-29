f79818c0fc9ac3aa8805b2679d66418a0fc58850
thori1222 <thori@LAPTOP-UTPCPP14.localdomain>
2021-04-29 15:45:46 +0900

    add build file

---
+++ build/CMakeFiles/mpc_cacc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mpc_cacc.dir/main.cpp.o"
+  "mpc_cacc.pdb"
+  "mpc_cacc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mpc_cacc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
